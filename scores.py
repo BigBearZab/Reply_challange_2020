a00cf5e216c4e142d3bff5958ebb1e8ed04c46e4
ogent <olivergent@btinternet.com>
2020-03-12 19:24:03 +0000

    wooo

--- scores.py
+++ scores.py
@@ -21,6 +21,33 @@ scores = {}
 for pair in combinations(total.keys(), 2):
     scores[pair[0] + "-" + pair[1]] = score_calc(total[pair[0]], total[pair[1]])
 
+# sort the scores
+scores_sorted = [k for k, v in sorted(scores.items(), key=lambda item: item[1])]
+
+
+# work out if a pair is manager-manager etc
+def relation(pair):
+    if pair[0][0] == "M" and pair[1][0] == "M":
+        return "M"
+    elif pair[0][0] == "D" and pair[1][0] == "D":
+        return "D"
+    else:
+        return "-"
+
+for i in all_pairs:
+    if relation(pair) == "M":
+        # get highest scoring manager score
+        
+        pass
+    elif relation(pair) == "D":
+        # get highest developer score
+        pass
+    else:
+        # get highest manager-developer score
+        pass
+
+
+
 # calculate manager combinations
 scores = {}
 for pair in combinations(manager_meta_labelled.keys(), 2):
@@ -30,3 +57,5 @@ for pair in combinations(manager_meta_labelled.keys(), 2):
 scores = {}
 for pair in combinations(dev_meta_labelled.keys(), 2):
     scores[pair[0] + "-" + pair[1]] = score_calc(total[pair[0]], total[pair[1]])
+
+
